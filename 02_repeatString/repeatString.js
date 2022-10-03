d98133f7972543834f2e22df4756c2dcd196b041
Aria <36065920+Fatin-Almalik@users.noreply.github.com>
2022-10-03 09:59:24 +0300

    Solve exercises #1, 2, 3, and 4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeats) {
+    output = ''
+    if (repeats < 0) {
+        output = 'ERROR'
+    }
+    for (let i = 0; i < repeats; i++) {
+        output = output + str;
+    }
+    return output;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    const args = Array.from(arguments); //to handle optional args
+    output = [...array]; //copy the input array to modify it later
 
+    for (let j = 0; j < args.length; j++) { //loop over the optional args
+        for (let i = 0; i < array.length; i++) { //loop over the array    
+            if (array[i] === args[j]) { //deep equality, so 1 != '1'
+                //remove args[i] from the output array
+                array.splice(i, 1);
+                break; //a matching element was found and removed
+            }
+            
+        }
+        
+    }
+
+    return array
 };
 
 // Do not edit below this line
